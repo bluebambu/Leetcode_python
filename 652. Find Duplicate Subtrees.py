9f3a9ad6bcd19348b2364957314e168c3e9b6364
bambu <electricitymouse@gmail.com>
2020-07-01 20:05:55 -0400

    l

--- 652. Find Duplicate Subtrees.py
+++ 652. Find Duplicate Subtrees.py
@@ -1,3 +1,5 @@
+from collections import deque
+from queue import Queue
 from typing import List
 
 
@@ -8,15 +10,75 @@ class TreeNode:
         self.right = right
 
 
+def lvlOrder(node: TreeNode) -> str:
+    q = deque()
+    q.append(node)
+    r = []
+    while q:
+        f = q.popleft()
+        r.append('#' if not f else str(f.val))
+        if f:
+            q.append(f.left)
+            q.append(f.right)
+    return ','.join(r)
+
+
+
+class Tree:
+    def __init__(self):
+        self.root = TreeNode()
+
+    def __init__(self, s: str):
+        self.root = self.deserialize(s)
+
+    def deserialize(self, s: str):
+        nodes = s.split(',')
+        i = 0
+        root = TreeNode(val=nodes[i])
+        i += 1
+        q = deque()
+        q.append(root)
+        while q and i<len(s):
+            f = q.popleft()
+            if nodes[i] != '#':
+                f.left = TreeNode(nodes[i])
+                q.append(f.left)
+            i += 1
+            if nodes[i] != '#':
+                f.right = TreeNode(nodes[i])
+                q.append(f.right)
+            i += 1
+
+        return root
+
 #
 class Solution:
     def findDuplicateSubtrees(self, root: TreeNode) -> List[TreeNode]:
-        r = []
+        r = set()
+        cache = {}
+
+        def postOrder(node: TreeNode, r):
+            if not node:
+                return '#'
+
+            serialize = str(node.val)+','+\
+                        postOrder(node.left, r)+','+\
+                        postOrder(node.right, r)
+            if serialize not in cache:
+                cache[serialize] = node
+            else:
+                r.add(cache[serialize])
+            return serialize
+
         postOrder(root, r)
         return r
 
 
 s = Solution()
-r = s.kthSmallest(matrix, k)
-print(r)
-assert r == 1
+tree = Tree('1,2,3,4,#,2,4,#,#,4,#,#,#,#,#')
+r = s.findDuplicateSubtrees(tree.root)
+for e in r:
+    print(lvlOrder(e))
+
+
+
